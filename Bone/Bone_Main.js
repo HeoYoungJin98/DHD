7ea4ea00c91b7fd3a83969ccf2828df43c0ad9ac
YoungJin.Heo <YoungJin.Heo98@gmail.com>
2022-10-02 18:34:38 +0900

    add some codes

--- Bone/Bone_Main.js
+++ Bone/Bone_Main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
             google.charts.load('current',{'packages':['corechart']});
             google.charts.setOnLoadCallback(function(){
                 Create_Chart('관절염 현재 유병 여부(대전시)',location, '무응답', No_Answer, '없음', No, '있음', Yes, '의사 진단 받지 않음', Not);
-                Create_Bar_Chart('관절염 환자 비율(단위: %)', location_Bar, '대전 관절염 환자 비율', Percent_Daejeon, '전국 관절염 환자 비율', Percent_All);
+                Create_Bar_Chart('관절염 환자 비율(단위: %)', location_Bar, '대전시 관절염 환자 비율', Percent_Daejeon, '전국 관절염 환자 비율', Percent_All);
             });
         }
     )
@@ -29,14 +29,16 @@ $(document).ready(function(){
             let Data = JSON.parse(data);
             let Man = parseInt(Data[0]);
             let Woman = parseInt(Data[1]);
+            let d_all = Man + Woman;
             let location = document.getElementById("Gender_Pie");
             let location_Bar = document.getElementById("Gender_Bar");
             let Man_All = 120; //From Excel Data =COUNTIFS(BR2:BR7360,1,I2:I7360,1)
             let Woman_All = 505; // =COUNTIFS(BR2:BR7360,1,I2:I7360,2)
+            let All = 625;
             google.charts.load('current',{'packages':['corechart']});
             google.charts.setOnLoadCallback(function(){
                 Create_Chart('관절염 환자 성비(단위: 명)', location, '남자', Man, '여자', Woman);
-                Create_Bar_Chart('관절염 환자 성비(단위: %)', location_Bar, '대전시 남자', Man/(Man+Woman)*100,'대전시 여자', Woman/(Man+Woman)*100, '전국 남자', Man_All/(Man_All+Woman_All)*100,'전국 여자', Woman_All/(Man_All+Woman_All)*100);
+                Create_Bar_Chart('관절염 환자 성비(단위: %)', location_Bar, '대전시 남자', avg(Man,d_all), '전국 남자', avg(Man_All,All),'대전시 여자', avg(Woman,d_all), '전국 여자', avg(Woman_All,All));
             });
         }
     )
@@ -47,6 +49,34 @@ $(document).ready(function(){
         {},
         function(data){
             let Data = JSON.parse(data);
+            let d30 = parseInt(Data[0]); //30-40
+            let d40 = parseInt(Data[1]); //40-50
+            let d50 = parseInt(Data[2]); //50-60
+            let d60 = parseInt(Data[3]); //60-70
+            let d70 = parseInt(Data[4]); //70-80
+            let d80 = parseInt(Data[5]); //over 80
+            let d_all = d30 + d40 + d50 + d60 + d70 + d80;
+            let location_Pie = document.getElementById("Age_Pie");
+            let location_Bar = document.getElementById("Age_bar");
+            let a20 = 2; //=COUNTIFS(BR2:BR7360,1,K2:K7360,20)
+            let a30 = 3; //==COUNTIFS(BR2:BR7360,1,K2:K7360,30)
+            let a40 = 16; //=COUNTIFS(BR2:BR7360,1,K2:K7360,40)
+            let a50 = 72; //=COUNTIFS(BR2:BR7360,1,K2:K7360,50)
+            let a60 = 222; //=COUNTIFS(BR2:BR7360,1,K2:K7360,60)
+            let a70 = 221; //=COUNTIFS(BR2:BR7360,1,K2:K7360,70)
+            let a80 = 89; //=COUNTIFS(BR2:BR7360,1,K2:K7360,80)
+            let a_all = a20 + a30 + a40 + a50 + a60 + a70 + a80;
+            google.charts.load('current',{'packages':['corechart']});
+            google.charts.setOnLoadCallback(function(){
+                Create_Chart('나이대별 관절염 환자 수',location_Pie, '20대',0,'30대',d30,'40대',d40,'50대',d50,'60대',d60,'70대',d70,'80대',d80)
+                Create_Bar_Chart('관절염 환자 수 비율(대전시, 전국)',location_Bar, '대전시 20대', 0, '전국 20대', avg(a20,a_all), '대전시 30대', avg(d30,d_all), '전국 30대', avg(a30,a_all), '대전시 40대', avg(d40,d_all), '전국 40대', avg(a40,a_all), '대전시 50대', avg(d50,d_all), '전국 50대', avg(a50, a_all), '대전시 60대', avg(d60,d_all), '전국 60대', avg(a60,a_all), '대전시 70대', avg(d70,d_all), '전국 70대',avg(a70,a_all), '대전시 80대', avg(d80,d_all), '전국 80대', avg(a80,a_all));
+            });
         }
     )
+    
+    
+    function avg(numerator, denominator){
+        let result = numerator/denominator*100;
+        return result;
+    }
 })
---
+++ Bone/Get_Rate_Age.php
@@ -0,0 +1,14 @@
+<?php
+include '../../Conn/Conn.php';
+
+$sql = "SELECT COUNT(*) FROM Health WHERE arthritis = 1 GROUP BY AGE";
+$Data = mysqli_query($db_conn, $sql);
+$Result_Array = array();
+
+while($Result = mysqli_fetch_array($Data)){
+    array_push($Result_Array, $Result[0]);
+}
+
+echo json_encode($Result_Array);
+
+mysqli_close($db_conn);
